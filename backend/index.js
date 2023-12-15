d4c66bad5c60e4aef150ba0d61e6790f894683cf
devham236 <90037750+devham236@users.noreply.github.com>
2023-12-15 15:06:02 +0100

    env file

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,6 @@ app.get("/horses", (req, res) => {
   }
 });
 
-app.listen(8000, () => {
-  console.log("Server is running on Port 8000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on Port ${process.env.PORT}`);
 });
